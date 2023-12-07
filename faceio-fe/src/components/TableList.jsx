bb62aa8a5f23558cc10a7020eeb34506b3b8b2a3
tsong0ku <sonbappe89@gmail.com>
2023-12-07 17:35:33 +0700

    complete add,get,delete

--- faceio-fe/src/components/TableList.jsx
+++ faceio-fe/src/components/TableList.jsx
@@ -2,69 +2,55 @@ import { Space, Table, Tag, Button, Modal, Input, Form } from "antd";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
 const TableList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [form] = Form.useForm();
-  const [editingStaff, setEditingStaff] = useState(null);
-  const showModal = () => {
+  const [editingStaff, isEditingStaff] = useState(null);
+  const [data, setData] = useState([]);
+  const showModal = (id) => {
     setIsModalOpen(true);
+    const onFinish = (values) => {
+      axios({
+        method: "PUT",
+        url: `http://localhost:3000/api/manager/${id}`,
+        data: values,
+        headers: {
+          "Content-Type": "application/json",
+          accept: "*/*",
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    };
   };
-  const handleEditStaff = () => {
+  const handleSubmitEditStaff = () => {
     setIsModalOpen(false);
   };
   const handleCancel = () => {
     setIsModalOpen(false);
     setEditingStaff(null);
   };
-  const [data, setData] = useState([
-    {
-      id: "1",
-      name: "Nguyễn Khánh",
-      age: 25,
-      email: "khanhit@mail.com",
-      tags: ["IT"],
-    },
-    {
-      id: "2",
 
-      name: "Nguyễn Quân Trường",
-      age: 35,
-      email: "truongnq@mail.com",
-      tags: ["Sales Manager"],
-    },
-    {
-      id: "3",
-      name: "Nguyễn Đức Trà",
-      age: 20,
-      email: "trand@mail.com",
-      tags: ["intern"],
-    },
-  ]);
-
-  // const save = async () => {
-  //   try {
-  //     const values = await form.validateFields();
-
-  //     // Perform the API call to update the information using onUpdateInfo
-  //     // Pass the updated information and key to the onUpdateInfo function
-  //     onUpdateInfo(editingKey, values);
-
-  //     setEditingKey("");
-  //     setModalVisible(false);
-  //     form.resetFields();
-  //   } catch (error) {
-  //     console.error("Validation failed", error);
-  //   }
-  // };
+  const base_url = "http://localhost:3000";
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${base_url}/api/manager`,
+    }).then((res) => {
+      setData(res.data.result);
+    });
+  }, []);
 
   const baseUrl = "http://localhost:3000";
   //delete function
   const handleDelete = (id) => {
     axios
       .delete(`${baseUrl}/api/manager/${id}`)
-      .then((response) => {
-        console.log("Success fetching data:", response);
+      .then(() => {
+        console.log("xoa thanh cong");
+        location.reload();
+
+        // setData(data.filter((item) => item.id != id));
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -72,6 +58,11 @@ const TableList = () => {
   };
 
   const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+      key: "_id",
+    },
     {
       title: "Tên",
       dataIndex: "name",
@@ -92,38 +83,21 @@ const TableList = () => {
     },
     {
       title: "Chức vụ",
-      key: "tags",
-      dataIndex: "tags",
-      render: (_, { tags }) => (
-        <>
-          {tags.map((tag) => {
-            let color = tag.length > 5 ? "geekblue" : "green";
-            if (tag === "intern") {
-              color = "volcano";
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
+      key: "position",
+      dataIndex: "position",
     },
     {
       title: "Action",
       key: "action",
-      render: () => (
+      render: (record) => (
         <Space size="middle">
-          <Button type="primary" onClick={showModal}>
+          <Button type="primary" onClick={showModal(record._id)}>
             Sửa
           </Button>
           <Button
             type="primary"
             danger
-            onClick={() => {
-              handleDelete(2);
-            }}
+            onClick={() => handleDelete(record._id)}
           >
             Xóa
           </Button>
@@ -132,18 +106,6 @@ const TableList = () => {
     },
   ];
 
-  // const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   axios
-  //     .get(`${baseUrl}/api/manager?page=1&limit=3&search=test`)
-  //     .then((response) => {
-  //       console.log("Success fetching data:", response);
-  //       setData(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
   return (
     <>
       <Table
@@ -159,9 +121,9 @@ const TableList = () => {
         title="Edit Information"
         open={isModalOpen}
         onCancel={handleCancel}
-        onOk={handleEditStaff}
+        onOk={handleSubmitEditStaff}
       >
-        <Form form={form} layout="vertical" name="editForm">
+        <Form form={form} layout="vertical" name="editForm" onFinish={onFinish}>
           <Form.Item
             name="name"
             label="Name"
@@ -184,12 +146,15 @@ const TableList = () => {
             <Input type="email" />
           </Form.Item>
           <Form.Item
-            name="job"
-            label="Job"
+            name="position"
+            label="position"
             rules={[{ required: true, message: "Job is required" }]}
           >
             <Input />
           </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
         </Form>
       </Modal>
     </>
